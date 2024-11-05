ccfefddf1b4c85c9b7ab1fec3b4700f9c857d196
Sainul Abiheen P <sainulabidheenpsainu@gmail.com>
2024-11-06 01:35:22 +0530

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,15 @@
+void main(){
+  myName();
+}
+void myName(){
+  Map<String,String>name={
+    "G WAGON": "MERCEDES",
+    "FORTUNER":"TOYOTA",
+    "ENDEVER":"FORD",
+    "PATROL":"NISSAN",
+    "PAJERO":"MITHSUBSHI",
+  };
+  name["THAVERA"]="CHEVEROLET";
+    print(name);
+print(name["PATROL"]);
+}
